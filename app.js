68d4b848021223605e5dc485adef9a75b79d1b49
Silhoue <Silhoue@users.noreply.github.com>
2016-06-06 00:19:34 +0200

    Add basic server files

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const port = 3000;
+
+const app = express();
+
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+app.listen(port, function () {
+  console.log("App listening on port " + port);
+});
