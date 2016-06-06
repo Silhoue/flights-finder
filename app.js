fb26717bcc2fdb7661222f5e68789b9feddbcfd0
Silhoue <Silhoue@users.noreply.github.com>
2016-06-06 19:55:09 +0200

    Send some json to the server

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
+var bodyParser = require('body-parser');
+
 const port = 3000;
 
 const app = express();
+app.disable("x-powered-by")
 
-app.get('/', function(req, res) {
-  res.send('hello world');
+app.use(bodyParser.json());
+
+app.use(express.static("src/public"));
+
+app.post("/", function(req, res) {
+	console.log(req.body);
+  	res.json({})
 });
 
 app.listen(port, function () {
